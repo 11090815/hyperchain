482510f76007bd3720cdda463be3c7412fed868c
11090815 <1378406814@mail.nwpu.edu.cn>
2023-10-22 15:52:36 +0800

    利用x509.MarshalPKCS8PrivateKey方法将ecdsa私钥转换为PKCS#8 ASN.1 DER格式

--- bccsp/keys.go
+++ bccsp/keys.go
@@ -2,49 +2,11 @@ package bccsp
 
 import (
 	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/x509"
-	"encoding/asn1"
 	"encoding/pem"
 	"errors"
 	"fmt"
 )
-
-type pkcs8Info struct {
-	Version             int
-	PrivateKeyAlgorithm []asn1.ObjectIdentifier
-	PrivateKey          []byte
-}
-
-type ecPrivateKey struct {
-	Version       int
-	PrivateKey    []byte
-	NamedCurveOID asn1.ObjectIdentifier `asn1:"optional,explicit,tag:0"`
-	PublicKey     asn1.BitString        `asn1:"optional,explicit,tag:1"`
-}
-
-var (
-	oidNamedCurveP224 = asn1.ObjectIdentifier{1, 3, 132, 0, 33}
-	oidNamedCurveP256 = asn1.ObjectIdentifier{1, 2, 840, 10045, 3, 1, 7}
-	oidNamedCurveP384 = asn1.ObjectIdentifier{1, 3, 132, 0, 34}
-	oidNamedCurveP521 = asn1.ObjectIdentifier{1, 3, 132, 0, 35}
-)
-
-var oidPublicKeyECDSA = asn1.ObjectIdentifier{1, 2, 840, 10045, 2, 1}
-
-func oidFromNamedCurve(curve elliptic.Curve) (asn1.ObjectIdentifier, bool) {
-	switch curve {
-	case elliptic.P224():
-		return oidNamedCurveP224, true
-	case elliptic.P256():
-		return oidNamedCurveP256, true
-	case elliptic.P384():
-		return oidNamedCurveP384, true
-	case elliptic.P521():
-		return oidNamedCurveP521, true
-	}
-	return nil, false
-}
 
 func privateKeyToPEM(privateKey interface{}) ([]byte, error) {
 	if privateKey == nil {
@@ -57,39 +19,15 @@ func privateKeyToPEM(privateKey interface{}) ([]byte, error) {
 			return nil, errors.New("invalid ecdsa private key: [it shouldn't be nil]")
 		}
 
-		oidNamedCurve, ok := oidFromNamedCurve(key.Curve)
-		if !ok {
-			return nil, errors.New("unknown elliptic curve")
-		}
-
-		privateKeyBytes := key.D.Bytes()
-		paddedPrivateKey := make([]byte, (key.Curve.Params().N.BitLen()+7)/8)
-		copy(paddedPrivateKey[len(paddedPrivateKey)-len(privateKeyBytes):], privateKeyBytes)
-
-		asn1Bytes, err := asn1.Marshal(ecPrivateKey{
-			Version:    1,
-			PrivateKey: paddedPrivateKey,
-			PublicKey:  asn1.BitString{Bytes: elliptic.Marshal(key.Curve, key.X, key.Y)},
-		})
+		privateKeyBytes, err := x509.MarshalPKCS8PrivateKey(key)
 		if err != nil {
-			return nil, fmt.Errorf("failed marshaling EC key to asn1: [%s]", err.Error())
+			return nil, fmt.Errorf("error marshaling ecdsa key to asn1: [%s]", err)
 		}
 
-		var pkcs8Key pkcs8Info
-		pkcs8Key.Version = 0
-		pkcs8Key.PrivateKeyAlgorithm = make([]asn1.ObjectIdentifier, 2)
-		pkcs8Key.PrivateKeyAlgorithm[0] = oidPublicKeyECDSA
-		pkcs8Key.PrivateKeyAlgorithm[1] = oidNamedCurve
-		pkcs8Key.PrivateKey = asn1Bytes
-
-		pkcs8Bytes, err := asn1.Marshal(pkcs8Key)
-		if err != nil {
-			return nil, fmt.Errorf("error marshaling EC key to asn1: [%s]", err)
-		}
 		return pem.EncodeToMemory(
 			&pem.Block{
 				Type:  "PRIVATE KEY",
-				Bytes: pkcs8Bytes,
+				Bytes: privateKeyBytes,
 			},
 		), nil
 
